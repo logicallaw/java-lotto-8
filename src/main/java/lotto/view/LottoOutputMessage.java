28ff4155a222993a300eb4e6b87b6d26e66006de
logicallaw <logicallawbio@gmail.com>
2025-11-03 12:56:23 +0900

    feat(view): LottoOutputMessage 정의

---
+++ src/main/java/lotto/view/LottoOutputMessage.java
@@ -0,0 +1,21 @@
+/*
+ * This is file of the project java-lotto-precourse
+ * Copyright (c) 2025 logicallaw
+ * Author: Junho Kim
+ * Latest Updated Date: 2025-11-03
+ */
+package lotto.view;
+
+public enum LottoOutputMessage {
+    LOTTO_QUANTITY_ISSUED("개를 구매했습니다.");
+
+    private final String label;
+
+    LottoOutputMessage(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+}
