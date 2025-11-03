48cd27f39b542234cbe13e0e0d7c5d8c18d44602
logicallaw <logicallawbio@gmail.com>
2025-11-03 20:17:01 +0900

    feat(view): 로또 추첨 결과 관련 상수 정의 및 화면 구현

--- src/main/java/lotto/view/LottoOutputMessage.java
+++ src/main/java/lotto/view/LottoOutputMessage.java
@@ -6,8 +6,19 @@
  */
 package lotto.view;
 
+/**
+ * 로또의 화면 출력 메시지를 정의한다.
+ */
 public enum LottoOutputMessage {
+    LOTTO_PROFIT_START("총 수익률은 "),
+    LOTTO_PROFIT_END("입니다."),
     LOTTO_DRAW_RESULT("당첨 통계"),
+    LOTTO_THREE_MATCH("3개 일치 (5,000원) - "),
+    LOTTO_FOUR_MATCH("4개 일치 (50,000원) - "),
+    LOTTO_FIVE_MATCH("5개 일치 (1,500,000원) - "),
+    LOTTO_FIVE_AND_BOUNS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    LOTTO_SIX_MATCH("6개 일치 (2,000,000,000원) - "),
+    NUMBER_UNIT("개"),
     LOTTO_QUANTITY_ISSUED("개를 구매했습니다.");
 
     private final String label;
