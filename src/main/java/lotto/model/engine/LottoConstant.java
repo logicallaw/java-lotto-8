cb37ef122fe916e702321363962b90a433997a05
logicallaw <logicallawbio@gmail.com>
2025-11-03 13:04:00 +0900

    feat(engine): enum LottoConstant 정의

---
+++ src/main/java/lotto/model/engine/LottoConstant.java
@@ -0,0 +1,29 @@
+/*
+ * This is file of the project java-lotto-precourse
+ * Copyright (c) 2025 logicallaw
+ * Author: Junho Kim
+ * Latest Updated Date: 2025-11-03
+ */
+package lotto.model.engine;
+
+/**
+ * 로또 발행과 관련된 상수를 정의한다.
+ */
+public enum LottoConstant {
+    MIN_NUMBER_OF_LOTTO(1),
+    MAX_NUMBER_OF_LOTTO(45),
+    START_LOTTO_NUMBER(1),
+    END_LOTTO_NUMBER(6),
+    THOUSAND_WON(1000),
+    ONLY_BE_DIVIDED_BY_THEMSELVES(0);
+
+    private final int label;
+
+    LottoConstant(int label) {
+        this.label = label;
+    }
+
+    public int getLabel() {
+        return this.label;
+    }
+}
